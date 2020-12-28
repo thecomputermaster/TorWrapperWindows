2c4c3ea3e8d6444c2573b0191f75ecbc98e24251
thecomputermaster <thecomputermaster@users.noreply.github.com>
2020-12-28 14:43:38 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace WindowsTorWrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ipmonitor = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorkerupdateorinstall = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorkerStartAlreadyConfigured = new System.ComponentModel.BackgroundWorker();
+            this.DisplayIP = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1254, 569);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "SocksPort 0\nORPort 4555\nNickname testrelay\nContactInfo kk@gmail.com\nLog notice fi" +
+    "le c:\\notices.log\nControlPort 9051\nExitPolicy reject *:*\n";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1246, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "C 2020 Keifmeister Technologies";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 631);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Update Torrc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 657);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "An example torrc is above";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(766, 631);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(476, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "If your torrc is already confgiured via this app click here to launch tor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(215, 631);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Add relay to Windows Startup";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(480, 631);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(254, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Stop relay from starting with Windows";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ipmonitor
+            // 
+            this.ipmonitor.Location = new System.Drawing.Point(0, 677);
+            this.ipmonitor.Name = "ipmonitor";
+            this.ipmonitor.Size = new System.Drawing.Size(391, 23);
+            this.ipmonitor.TabIndex = 10;
+            this.ipmonitor.Text = "Click to see IP addresses connecting to your relay";
+            this.ipmonitor.UseVisualStyleBackColor = true;
+            this.ipmonitor.Click += new System.EventHandler(this.ipmonitor_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorkerupdateorinstall
+            // 
+            this.backgroundWorkerupdateorinstall.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerupdateorinstall_DoWork);
+            // 
+            // backgroundWorkerStartAlreadyConfigured
+            // 
+            this.backgroundWorkerStartAlreadyConfigured.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerStartAlreadyConfigured_DoWork);
+            // 
+            // DisplayIP
+            // 
+            this.DisplayIP.DoWork += new System.ComponentModel.DoWorkEventHandler(this.DisplayIP_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 710);
+            this.Controls.Add(this.ipmonitor);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TorWrapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button ipmonitor;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerupdateorinstall;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerStartAlreadyConfigured;
+        private System.ComponentModel.BackgroundWorker DisplayIP;
+    }
+}
+
